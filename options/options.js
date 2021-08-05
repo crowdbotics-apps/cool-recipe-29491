cb7d38e096e5e6a2d8c709b6d02a6dda9edac773
Crowdbotics <team@crowdbotics.com>
2021-08-05 02:14:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "cool_recipe_29491Identifier",
+  url: "https://cool_recipe_29491Identifier.botics.co",
+  api: "https://cool_recipe_29491Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
